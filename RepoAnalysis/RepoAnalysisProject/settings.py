b2fdf793ac0b598bc5332bfd31115ea3343e72a4
GGuarino88 <79419206+GGuarino88@users.noreply.github.com>
2023-06-12 21:23:05 -0400

    HTML Static
    Added Django static files to results.html and changed the source for static files in settings.py

--- RepoAnalysis/RepoAnalysisProject/settings.py
+++ RepoAnalysis/RepoAnalysisProject/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 GIT_API_TOKEN = env("GIT_API_TOKEN")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 
@@ -127,10 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "RepoAnalysisApp/static/",
+    
 ]
 
 # Default primary key field type
