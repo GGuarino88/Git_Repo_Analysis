cb996e118028655355e7db678ce1f7110eb03edc
girik-met-bu <girik@bu.edu>
2023-07-10 14:55:31 -0400

    Fix Too Many Redirections and Remove csv file storing results
    Fix Too Many Redirections and Remove csv file storing results

--- RepoAnalysis/RepoAnalysisApp/urls.py
+++ RepoAnalysis/RepoAnalysisApp/urls.py
@@ -1,27 +1,19 @@
 from django.contrib import admin
-
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from allauth.socialaccount.providers.github import views as github_view
-
 from . import views
 
-urlpatterns = [
-    #path('login/', github_view.oauth2_login, name='login'),
-    
+urlpatterns = [    
     # admin view url
     path('admin/', admin.site.urls),
     
     # allauth views url
-    
     path('accounts/', include('allauth.urls')),
     
     # custom view url
     path('login/', views.repoAnalysisLogin, name='login'),
     path('logout/', views.repoAnalysisLogout, name='logout'),
-    
-    # django view url
-    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     
     # application views url
     path('', views.home, name='home'),
