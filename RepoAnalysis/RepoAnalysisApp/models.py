a3e78bb628bb2beabcedb9ee6808f56f4820ddf2
GGuarino88 <79419206+GGuarino88@users.noreply.github.com>
2023-07-11 21:55:20 -0400

    Created first table Scan for testing

--- RepoAnalysis/RepoAnalysisApp/models.py
+++ RepoAnalysis/RepoAnalysisApp/models.py
@@ -4,17 +4,9 @@ from django.contrib.auth import get_user_model
 class Scan(models.Model):
     
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    title = models.CharField(max_length = 200, primary_key=True)
+    title = models.CharField(max_length = 200)
     description = models.TextField(max_length=200)
     
     class Meta():
         db_table = "user_scan"
-        
-class Repo_Scanned(models.Model):
-    
-    scan = models.OneToOneField(Scan, on_delete=models.CASCADE)
-    repo_url = models.URLField(max_length = 200)
-    
-    class Meta():
-        db_table = "repo_scanned"
     
