ea83c2264d716a121b78eed576c0e055e3a61e49
sashankvarma2000 <sashankvarma2000@gmail.com>
2023-07-17 21:41:04 -0400

    Changed About page

--- RepoAnalysis/RepoAnalysisProject/settings.py
+++ RepoAnalysis/RepoAnalysisProject/settings.py
@@ -90,7 +90,7 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "RepoAnalysisApp/static/",
 ]
