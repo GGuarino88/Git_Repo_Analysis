7dfe2bdf8b0f98235bfaf7eb9ed85a18372fa63b
girik-met-bu <girik@bu.edu>
2023-08-01 22:36:26 -0400

    Added commits per branch
    Added below
    1. commits per branch
    2. Release table Data an Time Formatting
    3. Branch Hyperlink to open in new window
    4. Updated "Repository Name" to "Project Title"
    5. Added hyperlink to Contributors

--- RepoAnalysis/RepoAnalysisApp/utils/github_api.py
+++ RepoAnalysis/RepoAnalysisApp/utils/github_api.py
@@ -20,3 +20,34 @@ class GitHubAPI:
         endpoint = f"repos/{owner}/{repo}{method}"
         generic_data = self.make_authenticated_request(endpoint)
         return generic_data
+
+    def get_commit_count_per_branch(self, repository_url):
+        owner, repo = self.parse_repository_url(repository_url)
+        branches = self.get_code_generic(repository_url, "/branches")
+        commit_count_per_branch = {}
+        for branch in branches:
+            branch_name = branch['name']
+            branch_endpoint = f"repos/{owner}/{repo}/commits?sha={branch_name}&per_page=100"
+            commits = []
+            page = 1
+            while True:
+                commits_response = self.make_authenticated_request(branch_endpoint + f"&page={page}")
+                if not commits_response:
+                    break
+                commits.extend(commits_response)
+                page += 1
+            commit_count_per_branch[branch_name] = len(commits)
+        return commit_count_per_branch
+    
+    def get_pull_request_count_per_contributor(self, repository_url):
+        owner, repo = self.parse_repository_url(repository_url)
+        endpoint = f"repos/{owner}/{repo}/pulls?state=all"
+        pull_requests = self.make_authenticated_request(endpoint)
+        pull_request_count_per_contributor = {}
+        for pull_request in pull_requests:
+            contributor = pull_request['user']['login']
+            if contributor in pull_request_count_per_contributor:
+                pull_request_count_per_contributor[contributor] += 1
+            else:
+                pull_request_count_per_contributor[contributor] = 1
+        return pull_request_count_per_contributor
