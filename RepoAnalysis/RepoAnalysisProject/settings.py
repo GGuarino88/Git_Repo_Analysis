aef6aaf11537c16daeb39c9e08e2079e27f494b3
girik-met-bu <girik@bu.edu>
2023-08-06 11:47:06 -0400

    using https always instead of http

--- RepoAnalysis/RepoAnalysisProject/settings.py
+++ RepoAnalysis/RepoAnalysisProject/settings.py
@@ -14,6 +14,7 @@ environ.Env.read_env(env_file)
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
 DEBUG = "False"
 ALLOWED_HOSTS = [ 'gitrepoanalysis.onrender.com' ]
 CSRF_TRUSTED_ORIGINS = [ 'https://gitrepoanalysis.onrender.com' ]
